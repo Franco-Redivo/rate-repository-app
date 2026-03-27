3da6002e5280cea620e8de1eeaf876248dd7d0e4
Franco Redivo <franco161101@gmail.com>
2026-03-27 19:26:49 -0300

    feat: implement AuthStorage utility and integrate with SignIn component

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,8 +1,12 @@
 import Text from './Text';
 import { View, StyleSheet } from 'react-native';
 import SignInForm from './SignInForm';
+import useSignIn from '../hooks/useSignIn';
+import AuthStorage from '../utils/authStorage';
 
 const SignIn = () => {
+    const [signIn] = useSignIn();
+    const authStorage = new AuthStorage();
 
     const styles = StyleSheet.create({
         container: {
@@ -11,8 +15,16 @@ const SignIn = () => {
         },
     });
 
-    const onSubmit = (values) => {
-        console.log(values);
+    const onSubmit = async (values) => {
+        const { username, password } = values;
+        try {
+            const data = await signIn({ username, password });
+            await authStorage.setAccessToken(data);
+            console.log('Sign-in successful, access token :', await authStorage.getAccessToken());
+
+        } catch (error) {
+            console.error('Sign-in failed:', error);
+        }
     };
     return (
         <View style={styles.container}>
